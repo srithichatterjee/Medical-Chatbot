2003b3bd1b5b8ee56395680b9d5d0aba032d1876
Srithi <srithichatterjee@gmail.com>
2026-05-19 02:14:26 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical-chatbot",
+    version="0.1.0",
+    author="Srithi Chatterjee",
+    author_email="srithichatterjee@gmail.com",
+    packages=find_packages(),
+    install_requires=[] 
+)
